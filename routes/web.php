abfe4e44573abc9995991ba9f4ac809f8dec1f48
biplabuddin99 <biplabuddin99@github.com>
2022-11-20 17:44:52 +0600

    back

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\frontend\FrontendController;
+use App\Http\Controllers\backend\BackendController;
 
 /*
 |--------------------------------------------------------------------------
