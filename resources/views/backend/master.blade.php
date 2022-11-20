a95865fa47cf9949eb38708d1d21c3733f52a56f
biplabuddin99 <biplabuddin99@github.com>
2022-11-20 18:48:14 +0600

    change

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -620,19 +620,19 @@
     <script src="{{ asset('bassets/vendor/libs/jquery/jquery.js') }}"></script>
     <script src="{{ asset('bassets/vendor/libs/popper/popper.js') }}"></script>
     <script src="{{ asset('bassets/vendor/js/bootstrap.js') }}"></script>
-    <script src="{{ asset('bassets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="{{ asset('bassets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
 
-    <script src="../assets/vendor/js/menu.js"></script>
+    <script src="{{ asset('bassets/vendor/js/menu.js')}}"></script>
     <!-- endbuild -->
 
     <!-- Vendors JS -->
-    <script src="../assets/vendor/libs/apex-charts/apexcharts.js"></script>
+    <script src="{{ asset('bassets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
 
     <!-- Main JS -->
-    <script src="../assets/js/main.js"></script>
+    <script src="{{ asset('bassets/js/main.js') }}"></script>
 
     <!-- Page JS -->
-    <script src="../assets/js/dashboards-analytics.js"></script>
+    <script src="{{ asset('bassets/js/dashboards-analytics.js')}}"></script>
 
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
